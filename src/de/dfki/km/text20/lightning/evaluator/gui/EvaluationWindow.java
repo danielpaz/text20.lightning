2cd036091ca0d778ebb11d94801b661b36cdf569
Christoph Kaeding <Christoph Kaeding@localhost>
2011-03-15 13:15:57 +0100

    - minor changes

--- src/de/dfki/km/text20/lightning/evaluator/gui/EvaluationWindow.java
+++ src/de/dfki/km/text20/lightning/evaluator/gui/EvaluationWindow.java
@@ -24,11 +24,11 @@ public class EvaluationWindow {
         mainFrame = new JFrame();
         dialogPane = new JPanel();
         labelDescription = new JLabel();
-        checkBoxImages = new JCheckBox();
         checkBoxSummary = new JCheckBox();
-        buttonSelect = new JButton();
+        checkBoxImages = new JCheckBox();
         scrollPane1 = new JScrollPane();
         listFiles = new JList();
+        buttonSelect = new JButton();
         buttonRemove = new JButton();
         scrollPane2 = new JScrollPane();
         listDetectors = new JList();
@@ -46,32 +46,26 @@ public class EvaluationWindow {
             {
                 dialogPane.setBorder(Borders.DIALOG_BORDER);
                 dialogPane.setLayout(new GridBagLayout());
-                ((GridBagLayout)dialogPane.getLayout()).columnWidths = new int[] {70, 10, 55, 10, 70, 10, 55, 0};
-                ((GridBagLayout)dialogPane.getLayout()).rowHeights = new int[] {0, 10, 0, 10, 20, 10, 0, 10, 87, 10, 0, 0};
-                ((GridBagLayout)dialogPane.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0E-4};
-                ((GridBagLayout)dialogPane.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0E-4};
+                ((GridBagLayout)dialogPane.getLayout()).columnWidths = new int[] {70, 10, 35, 35, 10, 70, 0};
+                ((GridBagLayout)dialogPane.getLayout()).rowHeights = new int[] {0, 10, 20, 10, 80, 10, 0, 10, 80, 10, 0, 0};
+                ((GridBagLayout)dialogPane.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0E-4};
+                ((GridBagLayout)dialogPane.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
 
                 //---- labelDescription ----
                 labelDescription.setText("text");
-                dialogPane.add(labelDescription, new GridBagConstraints(0, 0, 7, 1, 0.0, 0.0,
-                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                    new Insets(0, 0, 0, 0), 0, 0));
-
-                //---- checkBoxImages ----
-                checkBoxImages.setText("write images");
-                dialogPane.add(checkBoxImages, new GridBagConstraints(0, 2, 3, 1, 0.0, 0.0,
+                dialogPane.add(labelDescription, new GridBagConstraints(0, 0, 6, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
 
                 //---- checkBoxSummary ----
                 checkBoxSummary.setText("write summary");
-                dialogPane.add(checkBoxSummary, new GridBagConstraints(4, 2, 3, 1, 0.0, 0.0,
+                dialogPane.add(checkBoxSummary, new GridBagConstraints(0, 2, 3, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
 
-                //---- buttonSelect ----
-                buttonSelect.setText("Select");
-                dialogPane.add(buttonSelect, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0,
+                //---- checkBoxImages ----
+                checkBoxImages.setText("write images");
+                dialogPane.add(checkBoxImages, new GridBagConstraints(3, 2, 3, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
 
@@ -82,13 +76,19 @@ public class EvaluationWindow {
                     listFiles.setVisibleRowCount(3);
                     scrollPane1.setViewportView(listFiles);
                 }
-                dialogPane.add(scrollPane1, new GridBagConstraints(2, 4, 5, 3, 0.0, 0.0,
+                dialogPane.add(scrollPane1, new GridBagConstraints(0, 4, 6, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+
+                //---- buttonSelect ----
+                buttonSelect.setText("Select");
+                dialogPane.add(buttonSelect, new GridBagConstraints(0, 6, 1, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
 
                 //---- buttonRemove ----
                 buttonRemove.setText("Remove");
-                dialogPane.add(buttonRemove, new GridBagConstraints(0, 6, 1, 1, 0.0, 0.0,
+                dialogPane.add(buttonRemove, new GridBagConstraints(2, 6, 2, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
 
@@ -96,7 +96,7 @@ public class EvaluationWindow {
                 {
                     scrollPane2.setViewportView(listDetectors);
                 }
-                dialogPane.add(scrollPane2, new GridBagConstraints(0, 8, 7, 1, 0.0, 0.0,
+                dialogPane.add(scrollPane2, new GridBagConstraints(0, 8, 6, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
 
@@ -105,12 +105,12 @@ public class EvaluationWindow {
                 dialogPane.add(buttonStart, new GridBagConstraints(0, 10, 1, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
-                dialogPane.add(progressBar, new GridBagConstraints(2, 10, 5, 1, 0.0, 0.0,
+                dialogPane.add(progressBar, new GridBagConstraints(2, 10, 4, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
             }
             mainFrameContentPane.add(dialogPane, BorderLayout.CENTER);
-            mainFrame.pack();
+            mainFrame.setSize(375, 335);
             mainFrame.setLocationRelativeTo(mainFrame.getOwner());
         }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
@@ -121,11 +121,11 @@ public class EvaluationWindow {
     protected JFrame mainFrame;
     private JPanel dialogPane;
     protected JLabel labelDescription;
-    protected JCheckBox checkBoxImages;
     protected JCheckBox checkBoxSummary;
-    protected JButton buttonSelect;
+    protected JCheckBox checkBoxImages;
     private JScrollPane scrollPane1;
     protected JList listFiles;
+    protected JButton buttonSelect;
     protected JButton buttonRemove;
     private JScrollPane scrollPane2;
     protected JList listDetectors;
